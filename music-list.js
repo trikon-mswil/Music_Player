27e694c79df690fe813ed39dcd38023cb2384d9b
trikon-mswil <151496115+trikon-mswil@users.noreply.github.com>
2023-12-30 09:57:02 +0530

    Create music-list.js

---
+++ music-list.js
@@ -0,0 +1,25 @@
+let allMusic = [
+    {
+      name: "Pal",
+      artist: "Arjit Singh",
+      img: "1",
+      src: "1"
+    },
+    {
+      name: "Falling",
+      artist: "Trevor Daniel",
+      img: "2",
+      src: "2"
+    },
+
+   
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember song must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
+
+
