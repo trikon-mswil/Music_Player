e8abc4da04431ffc8de8ddbef76fb99af3c21df8
trikon-mswil <151496115+trikon-mswil@users.noreply.github.com>
2023-12-30 10:13:57 +0530

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -1,25 +1,140 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
 let allMusic = [
     {
-      name: "Pal",
+      name: "Apna Bana Le",
       artist: "Arjit Singh",
       img: "1",
       src: "1"
     },
     {
-      name: "Falling",
-      artist: "Trevor Daniel",
+      name: "Players",
+      artist: "Badshah, Karan Aujla",
       img: "2",
       src: "2"
     },
-
-   
+    {
+      name: "Closer X Kesariya",
+      artist: "Sagar Swarap",
+      img: "3",
+      src: "3"
+    },
+    {
+      name: "Passori",
+      artist: "Coke Studio Pak",
+      img: "4",
+      src: "4"
+    },
+    {
+      name: "Dilawara",
+      artist: "The Prophec",
+      img: "5",
+      src: "5"
+    },
+    {
+      name: "Excuses",
+      artist: "AP Dhillon",
+      img: "6",
+      src: "6"
+    },
+    {
+      name: "Moon Rise",
+      artist: "Guru Randhawa",
+      img: "7",
+      src: "7"
+    },
+    {
+      name: "Tu Ake Dekh Le",
+      artist: "King",
+      img: "8",
+      src: "8"
+    },
+    {
+      name: "Mi Amor",
+      artist: "Sharn",
+      img: "9",
+      src: "9"
+    },
+    {
+      name: "Akhiyaan",
+      artist: "Mitraz",
+      img: "10",
+      src: "10"
+    },
+    {
+      name: "Nasha X Old Town Road",
+      artist: "Sush & Yohan",
+      img: "11",
+      src: "11"
+    },
+    {
+      name: "Ranjha",
+      artist: "B Praak",
+      img: "12",
+      src: "12"
+    },
+    {
+      name: "Safar",
+      artist: "Mix Singh",
+      img: "13",
+      src: "13"
+    },
+    {
+      name: "Indian Riddim",
+      artist: "Beats By Saheed",
+      img: "14",
+      src: "14"
+    },
+    {
+      name: "Rangeen",
+      artist: "Beats By Saheed",
+      img: "14",
+      src: "15"
+    },
+    {
+      name: "Noor",
+      artist: "Beats By Saheed",
+      img: "14",
+      src: "16"
+    },
+    {
+      name: "Karishma",
+      artist: "Beats By Saheed",
+      img: "14",
+      src: "17"
+    },
+    {
+      name: "Pachtaoge",
+      artist: "Arjit Singh",
+      img: "14",
+      src: "18"
+    },
+    {
+      name: "Indian Flute",
+      artist: "Beats By Saheed",
+      img: "14",
+      src: "19"
+    },
+    {
+      name: "Faasle",
+      artist: "Aditya Rikhari",
+      img: "14",
+      src: "20"
+    },
     // like this paste it and remember to give comma after ending of this bracket }
     // {
     //   name: "Here is the music name",
     //   artist: "Here is the artist name",
     //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-    //   src: "music name here - remember song must be in .mp3 formate and it's inside the songs folder of this project folder"
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
     // }
   ];
-
-
